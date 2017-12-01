e5e809698ec2be81750b00aeaa2c6f4d9dcfcadf
Emily Hong <emilyhong@college.harvard.edu>
2017-12-01 00:50:29 +0000

    added some templates and adjusted application.py, changed username in users to be unique field

--- application.py
+++ application.py
@@ -6,6 +6,8 @@ from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+from helpers import apology, login_required
+
 # Configure application
 app = Flask(__name__)
 
@@ -36,8 +38,40 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    # TODO
-    return None
+    """Register user"""
+    if request.method == "POST":
+        # check that all fields are filled
+        if not request.form.get("name"):
+            return apology("must provide name")
+        if not request.form.get("username"):
+            return apology("must provide username")
+
+        if not request.form.get("password"):
+            return apology("must provide password")
+        # check that confirmation is entered
+        if not request.form.get("confirmation"):
+            return apology("must provide confirmation")
+        # check that password matches confirmation
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("confirmation must match password")
+
+        # remember hashed form of password
+        hashPass = generate_password_hash(request.form.get("password"))
+
+        # input new user info into table users
+        result = db.execute("INSERT INTO users (name, username, hash) VALUES (:name, :username, :hashPass)",
+                            name=request.form.get("name"), username=request.form.get("username"), hashPass=hashPass)
+
+        # usernames are a unique field in users, return error if username already exists (execute will fail)
+        if not result:
+            return apology("username already taken")
+
+        # store user_id in session (to keep user logged in)
+        session["user_id"] = result
+
+        return redirect("/")
+    else: # if get method
+        return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -51,11 +85,11 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return error("must provide username", 403)
+            return apology("must provide username", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return error("must provide password", 403)
+            return apology("must provide password", 403)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
@@ -63,7 +97,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return error("invalid username and/or password", 403)
+            return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -90,10 +124,6 @@ def logout():
 def errorhandler(e):
     """Handle error"""
     # dunno how to do this or what this is for
-    return None
-
-def error(msg, code):
-    # take in error message (string) and output it in some format
     return None
 
 
